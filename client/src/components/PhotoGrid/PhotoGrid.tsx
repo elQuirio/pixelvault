e680c707e459e784bc6b4718eeaa8e9dacee0ac1
Filippo Quirino <filippo.quirino@hotmail.it>
2026-06-14 12:53:50 +0200

    feature(lightbox): add LightBox component to open pictures and close them with outside-click and esc button

--- client/src/components/PhotoGrid/PhotoGrid.tsx
+++ client/src/components/PhotoGrid/PhotoGrid.tsx
@@ -1,5 +1,7 @@
 import styles from "./PhotoGrid.module.css";
 import { Photo } from '../../api/upload';
+import { useState } from "react";
+import { LightBox } from "../LightBox/LightBox";
 
 type PhotoGridProps = {
   files: Photo[];
@@ -8,12 +10,14 @@ type PhotoGridProps = {
 const API_BASE = "http://localhost:3000";
 
 export function PhotoGrid({ files }: PhotoGridProps) {
+  const [selected, setSelected] = useState<Photo | null>(null);
 
   return (
     <div className={styles.gridContainer}>
       {files.map((u, i) => (
-        <img key={u.id} className={styles.thumbnail} src={`${API_BASE}${u.url}`} alt={files[i].id} />
+        <img key={u.id} className={styles.thumbnail} src={`${API_BASE}${u.url}`} alt={files[i].id} onClick={() => setSelected(u)} />
       ))}
+      {selected && <LightBox photo={selected} onClose={() => setSelected(null)}/>}
     </div>
   );
 }
