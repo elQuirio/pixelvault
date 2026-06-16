aa68890fcb42a89fecf769a293c884a320710dc8
Filippo Quirino <filippo.quirino@hotmail.it>
2026-06-16 21:10:08 +0200

    feature(lightbox): add arrow navigation into LightBox component

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,7 +23,7 @@ const app = Fastify({
   },
 });
 
-await app.register(cors, { origin: "http://localhost:5173", methods: '*' });
+await app.register(cors, { origin: "http://localhost:5173", methods: "*" });
 
 await app.register(multipart, {
   limits: {
@@ -39,11 +39,11 @@ await app.register(staticPlugin, {
 
 /////////////////////////////////////////////////////////////////////////////
 
-app.get("/health", async () => {
+app.get("/health", () => {
   return { status: "ok" };
 });
 
-app.get("/photos", async (req) => {
+app.get("/photos", async () => {
   const files = await readdir(ORIGINAL_DIR);
   return {
     photos: files
@@ -90,10 +90,8 @@ app.post("/upload", async (req) => {
   return { uploaded: saved };
 });
 
-
-
-app.delete('/photos/:id', async (req, reply) => {
-  const { id } = req.params as {id: string};
+app.delete("/photos/:id", async (req, reply) => {
+  const { id } = req.params as { id: string };
 
   if (!id) {
     return reply.code(400).send();
@@ -103,17 +101,41 @@ app.delete('/photos/:id', async (req, reply) => {
     //original
     await unlink(join(ORIGINAL_DIR, id));
     //thumbnail
-    await unlink(join(THUMBNAIL_DIR, `${id.split('.')[0]}.webp`));
+    await unlink(join(THUMBNAIL_DIR, `${id.split(".")[0]}.webp`));
     return reply.code(204).send();
   } catch (err) {
     const e = err as NodeJS.ErrnoException;
     if (e.code === "ENOENT") {
-      return reply.code(404).send( {message: 'Resource not found'} );
+      return reply.code(404).send({ message: "Resource not found" });
     } else {
-      return reply.code(500).send( {message: 'Internal server error'} );
+      return reply.code(500).send({ message: "Internal server error" });
     }
   }
 });
+
+app.delete("/photos", async (req, reply) => {
+  const { ids } = req.body as { ids: string[] };
+
+  if (!ids || ids.length === 0) {
+    return reply.code(400).send();
+  }
+
+  for (const id of ids) {
+      await safeUnlink(join(ORIGINAL_DIR, id));
+      await safeUnlink(join(THUMBNAIL_DIR, `${id.split(".")[0]}.webp`));
+    }
+  return reply.code(204).send();
+});
+
+
+async function safeUnlink(path: string) {
+  try {
+    await unlink(path);
+  } catch (err) {
+    const e = err as NodeJS.ErrnoException;
+    if (e.code !== "ENOENT") throw err;
+  }
+}
 
 //////////////////////////////////////////////////////////////////
 
@@ -126,4 +148,4 @@ const start = async () => {
   }
 };
 
-start();
+void start();
