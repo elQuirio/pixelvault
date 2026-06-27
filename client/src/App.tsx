8c916fe29bfdd0c7531a0f0195d39427855b29d6
Filippo Quirino <filippo.quirino@hotmail.it>
2026-06-27 21:48:29 +0200

    feature(auth): add auth screen gate + login and registration form scaffold + Gallery component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,51 +1,19 @@
-import { useState, useEffect } from "react";
-import { UploadArea } from "./components/UploadArea/UploadArea";
-import { PhotoGrid } from "./components/PhotoGrid/PhotoGrid";
-import { uploadFiles, getPhotos } from "./api/upload";
-import type { Photo } from "./api/upload";
-import { deletePhoto, deletePhotosBulk } from "./api/upload";
+
+import { useAuth } from './context/AuthContext.tsx';
+import { AuthScreen } from "./components/auth/AuthScreen";
+import { Gallery } from './components/Gallery/Gallery.tsx';
 
 function App() {
-  const [files, setFiles] = useState<Photo[]>([]);
-  const [isUploading, setIsUploading] = useState(false);
-  const [sortBy, setSortBy] = useState('creationDateDesc');
+  const {user, loading} = useAuth();
 
-  useEffect(() => {
-    getPhotos(sortBy).then((res) => {
-      setFiles(res.photos);
-    });
-  }, [sortBy]);
+  if (loading) return <p>Loading...</p>;
+  if (!user) return <AuthScreen />;
 
-  async function handleUploadFiles(newFiles: File[]) {
-    setIsUploading(true);
-    try {
-      await uploadFiles(newFiles);
-      const res = await getPhotos(sortBy);
-      console.log("Uploaded:", res);
-      setFiles(res.photos);
-    } catch (err) {
-      console.error("Upload failed:", err);
-    } finally {
-      setIsUploading(false);
-    }
-  }
-
-  async function handleDeletePhoto(id: string) {
-    await deletePhoto(id);
-    setFiles(files.filter((f) => f.id !== id));
-  }
-
-  async function handleDeleteBulkClick(ids: string[]) {
-    await deletePhotosBulk(ids);
-    setFiles(files.filter((f) => !ids.includes(f.id)));
-  }
 
   return (
     <main className="main-container">
       <h1>PixelVault</h1>
-      <UploadArea onFilesSelected={handleUploadFiles} />
-      {isUploading && <p className="status">Uploading...</p>}
-      <PhotoGrid files={files} handleDeletePhoto={handleDeletePhoto} handleDeleteBulkClick={handleDeleteBulkClick} sortBy={sortBy} setSortBy={setSortBy} />
+      <Gallery />
     </main>
   );
 }
---
+++ client/src/components/Gallery/Gallery.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import { UploadArea } from '../UploadArea/UploadArea.tsx'
+import { PhotoGrid } from "../PhotoGrid/PhotoGrid.tsx";
+import { uploadFiles, getPhotos } from "../../api/upload.ts";
+import type { Photo } from "../..//api/upload.ts";
+import { deletePhoto, deletePhotosBulk } from "../../api/upload.ts";
+
+export function Gallery() {
+  const [files, setFiles] = useState<Photo[]>([]);
+  const [isUploading, setIsUploading] = useState(false);
+  const [sortBy, setSortBy] = useState("creationDateDesc");
+
+  useEffect(() => {
+    getPhotos(sortBy).then((res) => {
+      setFiles(res.photos);
+    });
+  }, [sortBy]);
+
+  async function handleUploadFiles(newFiles: File[]) {
+    setIsUploading(true);
+    try {
+      await uploadFiles(newFiles);
+      const res = await getPhotos(sortBy);
+      console.log("Uploaded:", res);
+      setFiles(res.photos);
+    } catch (err) {
+      console.error("Upload failed:", err);
+    } finally {
+      setIsUploading(false);
+    }
+  }
+
+  async function handleDeletePhoto(id: string) {
+    await deletePhoto(id);
+    setFiles(files.filter((f) => f.id !== id));
+  }
+
+  async function handleDeleteBulkClick(ids: string[]) {
+    await deletePhotosBulk(ids);
+    setFiles(files.filter((f) => !ids.includes(f.id)));
+  }
+
+  return (
+    <>
+      <UploadArea onFilesSelected={handleUploadFiles} />
+      {isUploading && <p className="status">Uploading...</p>}
+      <PhotoGrid
+        files={files}
+        handleDeletePhoto={handleDeletePhoto}
+        handleDeleteBulkClick={handleDeleteBulkClick}
+        sortBy={sortBy}
+        setSortBy={setSortBy}
+      />
+    </>
+  );
+}
