b1c9448a8adc51e1b8cc2df2381876b131716669
Filippo Quirino <filippo.quirino@hotmail.it>
2026-06-29 16:35:05 +0200

    feature(auth): login form error toasts with custom useErrorToast hook

--- client/src/components/auth/RegistrationForm.tsx
+++ client/src/components/auth/RegistrationForm.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { register } from "../../api/auth";
+import styles from './RegistrationForm.module.css';
+import { useErrorToast } from "../../hooks/useErrorToast";
+
 
 type RegistrationFormProps = {
   setIsRegistration: (v: boolean) => void;
@@ -9,15 +12,24 @@ export function RegistrationForm({ setIsRegistration }: RegistrationFormProps) {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useErrorToast();
 
 
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
+    setError(null);
     const resp = await register({name: username, email, password });
     if (resp.ok) {
       setIsRegistration(false);
     } else {
-      console.log('error')
+      switch(resp.error) {
+        case 'invalid_input':
+          setError('Invalid input');
+          break;
+        case 'name_taken':
+          setError('Name already taken');
+          break;
+      }
     }
   }
 
@@ -25,6 +37,7 @@ export function RegistrationForm({ setIsRegistration }: RegistrationFormProps) {
   return (
     <form onSubmit={handleSubmit}>
       <p>Registration</p>
+      {error && <p className={styles.errorToast}>{error}</p>}
       <label htmlFor="registration-username">Username</label>
       <input id="registration-username" type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
       <label htmlFor="registration-email">Email</label>
