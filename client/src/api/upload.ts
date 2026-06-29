cc3f0476e96638d0bd2b4938f4fa27a931c90ef8
Filippo Quirino <filippo.quirino@hotmail.it>
2026-06-29 19:50:59 +0200

    feature(soft delete): addclient helper functions to handle soft delete

--- client/src/api/upload.ts
+++ client/src/api/upload.ts
@@ -58,6 +58,20 @@ export async function getPhotos(sortBy: string) {
   return res.json() as Promise<PhotosResponse>;
 }
 
+export async function getTrash(sortBy: string) {
+  const res = await fetch(`${API_BASE}/photos/trash?sortBy=${sortBy}`, {
+    method: "GET",
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    throw new Error(`Error fetching files: ${res.status} ${res.statusText}`);
+  }
+
+  return res.json() as Promise<PhotosResponse>;
+}
+
+
 export async function deletePhoto(id: string) {
   const res = await fetch(`${API_BASE}/photos/${id}`, {
     method: "DELETE",
@@ -82,5 +96,57 @@ export async function deletePhotosBulk(ids: string[]) {
     throw new Error(`Error deleting ids`);
   }
 
+  return;
+}
+
+export async function restorePhoto(id: string) {
+  const resp = await fetch(`${API_BASE}/photos/${id}/restore`,{
+    method: 'POST',
+    credentials: 'include',
+  });
+  if (!resp.ok) {
+    throw new Error(`Error deleting id: ${id}`);
+  }
+  return;
+}
+
+export async function restorePhotoBulk(ids: string[]) {
+  const resp = await fetch(`${API_BASE}/photos/restore`,{
+    method: 'POST',
+    headers: { "Content-type": "application/json" },
+    credentials: 'include',
+    body: JSON.stringify({ ids: ids }),
+  });
+
+  if (!resp.ok) {
+    throw new Error(`Error restoring ids`);
+  }
+  return;
+}
+
+export async function permanentDelete(id: string) {
+  const resp = await fetch(`${API_BASE}/photos/${id}/permanent`,{
+    method: 'DELETE',
+    credentials: 'include',
+  });
+
+  if (!resp.ok) {
+    throw new Error(`Error deleting id: ${id}`);
+  }
+  return;
+}
+
+
+export async function permanentDeleteBulk(ids: string[]) {
+  const resp = await fetch(`${API_BASE}/photos/permanent`,{
+    method: 'DELETE',
+    headers: { "Content-type": "application/json" },
+    credentials: 'include',
+    body: JSON.stringify({ ids: ids }),
+  });
+
+  if (!resp.ok) {
+    throw new Error(`Error deleting ids`);
+  }
   return;
 }
